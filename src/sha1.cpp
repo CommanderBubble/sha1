f0f75e92a60de48b119f2e3ef04a7bbb04d70ba4
Michael Lloyd <commander.bubble@gmail.com>
2015-02-16 22:07:12 +1100

    moving to same structure as MD5 library

--- src/sha1.cpp
+++ src/sha1.cpp
@@ -68,6 +68,166 @@ namespace sha1 {
 
     // Constructor *******************************************************
     sha1_t::sha1_t() {
+        initialise();
+    }
+
+    // Constructor *******************************************************
+    sha1_t::sha1_t(const char* buffer, const unsigned int buf_len, void* signature_) {
+        initialise();
+
+        process(buffer, buf_len);
+
+        finish(signature);
+    }
+
+    // process ***********************************************************
+    void sha1_t::process_block() {
+        if (unprocessedBytes == sha1::BLOCK_SIZE) {
+            //assert( unprocessedBytes == 64 );
+            //printf( "process: " ); hexPrinter( bytes, 64 ); printf( "\n" );
+
+            int t;
+            unsigned int a, b, c, d, e, K, f, W[80];
+
+            // starting values
+            a = H0;
+            b = H1;
+            c = H2;
+            d = H3;
+            e = H4;
+
+            // copy and expand the message block
+            for (t = 0; t < 16; t++)
+                W[t] = (bytes[t * 4]     << 24)
+                     + (bytes[t * 4 + 1] << 16)
+                     + (bytes[t * 4 + 2] << 8)
+                     +  bytes[t * 4 + 3];
+
+            for (; t < 80; t++)
+                W[t] = lrot(W[t - 3] ^ W[t - 8] ^ W[t - 14] ^ W[t - 16], 1);
+
+            /* main loop */
+            unsigned int temp;
+            for (t = 0; t < 80; t++) {
+                if (t < 20) {
+                    K = 0x5a827999;
+                    f = (b & c) | ((b ^ 0xFFFFFFFF) & d); //TODO: try using ~
+                } else if (t < 40) {
+                    K = 0x6ed9eba1;
+                    f = b ^ c ^ d;
+                } else if (t < 60) {
+                    K = 0x8f1bbcdc;
+                    f = (b & c) | (b & d) | (c & d);
+                } else {
+                    K = 0xca62c1d6;
+                    f = b ^ c ^ d;
+                }
+
+                temp = lrot(a, 5) + f + e + W[t] + K;
+                e = d;
+                d = c;
+                c = lrot(b, 30);
+                b = a;
+                a = temp;
+            }
+
+            /* add variables */
+            H0 += a;
+            H1 += b;
+            H2 += c;
+            H3 += d;
+            H4 += e;
+
+            /* all bytes have been processed */
+            unprocessedBytes = 0;
+        }
+    }
+
+    // addBytes **********************************************************
+    void sha1_t::process(const char* buffer, int buf_len) {
+        assert( buffer );
+
+        if (buf_len <= 0)
+            return;
+
+        // add these bytes to the running total
+        size += buf_len;
+
+        // repeat until all data is processed
+        while (buf_len > 0) {
+            // number of bytes required to complete block
+            int needed = sha1::BLOCK_SIZE - unprocessedBytes;
+            assert(needed > 0);
+
+            // number of bytes to copy (use smaller of two)
+            int toCopy = (buf_len < needed) ? buf_len : needed;
+
+            // Copy the bytes
+            memcpy(bytes + unprocessedBytes, buffer, toCopy);
+
+            // Bytes have been copied
+            buf_len -= toCopy;
+            buffer += toCopy;
+            unprocessedBytes += toCopy;
+
+            // there is a full block
+            if (unprocessedBytes == sha1::BLOCK_SIZE)
+                process_block();
+        }
+    }
+
+    // digest ************************************************************
+    void sha1_t::finish(void* signature_) {
+        // save the message size
+        unsigned int totalBitsL = size << 3;
+        unsigned int totalBitsH = size >> 29;
+
+        // add 0x80 to the message
+        process("\x80", 1);
+
+        unsigned char footer[sha1::BLOCK_SIZE] = {
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+        // block has no room for 8-byte filesize, so finish it
+        if( unprocessedBytes > 56 )
+            process((char*)footer, sha1::BLOCK_SIZE - unprocessedBytes);
+
+        assert( unprocessedBytes <= 56 );
+
+        // how many zeros do we need
+        int neededZeros = 56 - unprocessedBytes;
+
+        // store file size (in bits) in big-endian format
+        storeBigEndianUint32( footer + neededZeros    , totalBitsH );
+        storeBigEndianUint32( footer + neededZeros + 4, totalBitsL );
+
+        // finish the final block
+        process((char*)footer, neededZeros + 8);
+
+        // allocate memory for the digest bytes
+//        unsigned char* digest = (unsigned char*)malloc( 20 );
+
+        // copy the digest bytes
+/*        storeBigEndianUint32( digest, H0 );
+        storeBigEndianUint32( digest + 4, H1 );
+        storeBigEndianUint32( digest + 8, H2 );
+        storeBigEndianUint32( digest + 12, H3 );
+        storeBigEndianUint32( digest + 16, H4 );
+*/
+        storeBigEndianUint32(signature,      H0);
+        storeBigEndianUint32(signature + 4,  H1);
+        storeBigEndianUint32(signature + 8,  H2);
+        storeBigEndianUint32(signature + 12, H3);
+        storeBigEndianUint32(signature + 16, H4);
+
+        // return the digest
+        memcpy(signature_, signature, SHA1_SIZE);
+    }
+
+    void sha1_t::initialise() {
         // make sure that the data type is the right size
         assert(SHA1_SIZE == 20);
 
@@ -77,137 +237,11 @@ namespace sha1 {
         H2 = 0x98badcfe;
         H3 = 0x10325476;
         H4 = 0xc3d2e1f0;
+
         unprocessedBytes = 0;
         size = 0;
-    }
 
-    /** is this actually needed? */
-    // Destructor ********************************************************
-    sha1_t::~sha1_t() {
-        // erase data
-        H0 = H1 = H2 = H3 = H4 = 0;
-        for( int c = 0; c < 64; c++ ) bytes[c] = 0;
-        unprocessedBytes = size = 0;
-    }
-
-    // process ***********************************************************
-    void sha1_t::process() {
-        assert( unprocessedBytes == 64 );
-        //printf( "process: " ); hexPrinter( bytes, 64 ); printf( "\n" );
-        int t;
-        unsigned int a, b, c, d, e, K, f, W[80];
-        // starting values
-        a = H0;
-        b = H1;
-        c = H2;
-        d = H3;
-        e = H4;
-        // copy and expand the message block
-        for( t = 0; t < 16; t++ ) W[t] = (bytes[t*4] << 24)
-                                        +(bytes[t*4 + 1] << 16)
-                                        +(bytes[t*4 + 2] << 8)
-                                        + bytes[t*4 + 3];
-        for(; t< 80; t++ ) W[t] = lrot( W[t-3]^W[t-8]^W[t-14]^W[t-16], 1 );
-
-        /* main loop */
-        unsigned int temp;
-        for( t = 0; t < 80; t++ )
-        {
-            if( t < 20 ) {
-                K = 0x5a827999;
-                f = (b & c) | ((b ^ 0xFFFFFFFF) & d);//TODO: try using ~
-            } else if( t < 40 ) {
-                K = 0x6ed9eba1;
-                f = b ^ c ^ d;
-            } else if( t < 60 ) {
-                K = 0x8f1bbcdc;
-                f = (b & c) | (b & d) | (c & d);
-            } else {
-                K = 0xca62c1d6;
-                f = b ^ c ^ d;
-            }
-            temp = lrot(a,5) + f + e + W[t] + K;
-            e = d;
-            d = c;
-            c = lrot(b,30);
-            b = a;
-            a = temp;
-            //printf( "t=%d %08x %08x %08x %08x %08x\n",t,a,b,c,d,e );
-        }
-        /* add variables */
-        H0 += a;
-        H1 += b;
-        H2 += c;
-        H3 += d;
-        H4 += e;
-        //printf( "Current: %08x %08x %08x %08x %08x\n",H0,H1,H2,H3,H4 );
-        /* all bytes have been processed */
-        unprocessedBytes = 0;
-    }
-
-    // addBytes **********************************************************
-    void sha1_t::addBytes( const char* data, int num ) {
-        assert( data );
-
-        if (num <= 0)
-            return;
-
-        // add these bytes to the running total
-        size += num;
-        // repeat until all data is processed
-        while( num > 0 )
-        {
-            // number of bytes required to complete block
-            int needed = 64 - unprocessedBytes;
-            assert( needed > 0 );
-            // number of bytes to copy (use smaller of two)
-            int toCopy = (num < needed) ? num : needed;
-            // Copy the bytes
-            memcpy( bytes + unprocessedBytes, data, toCopy );
-            // Bytes have been copied
-            num -= toCopy;
-            data += toCopy;
-            unprocessedBytes += toCopy;
-
-            // there is a full block
-            if( unprocessedBytes == 64 ) process();
-        }
-    }
-
-    // digest ************************************************************
-    unsigned char* sha1_t::getDigest() {
-        // save the message size
-        unsigned int totalBitsL = size << 3;
-        unsigned int totalBitsH = size >> 29;
-        // add 0x80 to the message
-        addBytes( "\x80", 1 );
-
-        unsigned char footer[64] = {
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        // block has no room for 8-byte filesize, so finish it
-        if( unprocessedBytes > 56 )
-            addBytes( (char*)footer, 64 - unprocessedBytes);
-        assert( unprocessedBytes <= 56 );
-        // how many zeros do we need
-        int neededZeros = 56 - unprocessedBytes;
-        // store file size (in bits) in big-endian format
-        storeBigEndianUint32( footer + neededZeros    , totalBitsH );
-        storeBigEndianUint32( footer + neededZeros + 4, totalBitsL );
-        // finish the final block
-        addBytes( (char*)footer, neededZeros + 8 );
-        // allocate memory for the digest bytes
-        unsigned char* digest = (unsigned char*)malloc( 20 );
-        // copy the digest bytes
-        storeBigEndianUint32( digest, H0 );
-        storeBigEndianUint32( digest + 4, H1 );
-        storeBigEndianUint32( digest + 8, H2 );
-        storeBigEndianUint32( digest + 12, H3 );
-        storeBigEndianUint32( digest + 16, H4 );
-        // return the digest
-        return digest;
+        finished = false;
     }
 
 } // namespace sha1
--- src/sha1.h
+++ src/sha1.h
@@ -9,6 +9,7 @@ accompanying LICENSE file.
 */
 
 #ifndef SHA1_HEADER
+#define SHA1_HEADER
 
 /*
  *
@@ -18,30 +19,92 @@ accompanying LICENSE file.
  * this should be 20.
  *
  */
-const unsigned int SHA1_SIZE = 5 * sizeof(unsigned int);
+const unsigned int SHA1_SIZE = (5 * sizeof(unsigned int));
 
 namespace sha1 {
 
+
     const char* HEX_STRING = "0123456789abcdef"; // to convert to hex
+
+    /*
+     * The MD5 algorithm works on blocks of characters of 64 bytes.  This
+     * is an internal value only and is not necessary for external use.
+     */
+    const unsigned int BLOCK_SIZE = 64;
 
     class sha1_t {
         public:
+                        /*
+             * sha1_t
+             *
+             * DESCRIPTION:
+             *
+             * Initialize structure containing state of MD5 computation. (RFC 1321,
+             * 3.3: Step 3).  This is for progressive MD5 calculations only.  If
+             * you have the complete string available, call it as below.
+             * process should be called for each bunch of bytes and after the last
+             * process call, finish should be called to get the signature.
+             *
+             * RETURNS:
+             *
+             * None.
+             *
+             * ARGUMENTS:
+             *
+             * None.
+             */
             sha1_t();
-            ~sha1_t();
 
-            void addBytes(const char*, int);
+            /*
+             * sha1_t
+             *
+             * DESCRIPTION:
+             *
+             * This function is used to calculate a SHA1 signature for a buffer of
+             * bytes.  If you only have part of a buffer that you want to process
+             * then sha1_t, process, and finish should be used.
+             *
+             * RETURNS:
+             *
+             * None.
+             *
+             * ARGUMENTS:
+             *
+             * buffer - A buffer of bytes whose SHA1 signature we are calculating.
+             *
+             * buf_len - The length of the buffer.
+             *
+             * signature_ - A 20 byte buffer that will contain the MD5 signature.
+             */
+            sha1_t(const char* buffer, const unsigned int buf_len, void* signature_);
+
+            void process(const char*, int);
+            void finish(void* signature_ = NULL);
             unsigned char* getDigest();
+
         private:
             // utility methods
+            void initialise();
+
             unsigned int lrot(unsigned int, int);
             void storeBigEndianUint32(unsigned char*, unsigned int);
-            void process();
+            void process_block();
 
             // fields
-            unsigned int H0, H1, H2, H3, H4;
+            unsigned int H0;
+            unsigned int H1;
+            unsigned int H2;
+            unsigned int H3;
+            unsigned int H4;
+
             unsigned char bytes[64];
             int unprocessedBytes;
             size_t size;
+
+            unsigned char signature[SHA1_SIZE];           /* stored signature */
+            //char str[33];                       /* stored plain text hash */
+
+            bool finished;
     };
 
     void hex_printer(unsigned char*, int);
@@ -49,5 +112,5 @@ namespace sha1 {
 
 } // namescpace sha1
 
-#define SHA1_HEADER
+
 #endif
